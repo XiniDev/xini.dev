810bc9c1406b626d768063e57a1134e413ce4f26
Anthony Leung <anthonyleung07@gmail.com>
2023-11-08 01:41:11 +0000

    updated website
    made a simple about me section and a blank project section for now
    
    also fixed the text to center cuz it looks better :)
    
    it currently doesnt have much content but am just attempting css animations

--- src/routes/Header.tsx
+++ src/routes/Header.tsx
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import Xini_Icon_White from '../images/xini-icon-white.png';
 import Menu_Icon from '../images/menu-icon.png';
 
 const Header = () => {
+    const [menuActive, setMenuActive] = useState(false);
+
+    const menuClick = (e: React.MouseEvent<HTMLDivElement>) => {
+        e.stopPropagation();
+        if (menuActive) {
+            setMenuActive(false)
+        } else {
+            setMenuActive(true)
+        }
+    };
+
     return (
         <div className="header-wrapper">
             <div className="header-icon">
-                <img src={Xini_Icon_White}/>
+                <img src={Xini_Icon_White} alt="XiniDev Icon"/>
                 XINI
             </div>
-            <div className="header-menu">
-                <img src={Menu_Icon}/>
+            <div
+                className={`header-menu ${menuActive ? 'active' : ''}`}
+                onClick={(e) => menuClick(e)}
+            >
+                <img src={Menu_Icon} alt="Menu Icon"/>
             </div>
         </div>
     );
--- src/routes/Homepage.tsx
+++ src/routes/Homepage.tsx
@@ -1,19 +1,93 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './Header';
 
+import Xini_Icon_White from '../images/xini-icon-white.png';
+
 const Homepage = () => {
+    const [fadeDivs, setFadeDivs] = useState([false, false, false]);
+
+    const [hideDivs, setHideDivs] = useState([false, true, true]);
+
+    useEffect(() => {
+        const timeout1 = setTimeout(() => {
+            setFadeDivs((prev) => [true, prev[1], prev[2]]);
+        }, 300);
+        const timeout2 = setTimeout(() => {
+            setFadeDivs((prev) => [prev[0], true, prev[2]]);
+        }, 1200);
+        const timeout3 = setTimeout(() => {
+            setFadeDivs((prev) => [prev[0], prev[1], true]);
+        }, 2000);
+      
+        return () => {
+            clearTimeout(timeout1);
+            clearTimeout(timeout2);
+            clearTimeout(timeout3);
+        };
+    }, []);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const thresholds = [400, 800, 1200];
+
+            if (window.scrollY < thresholds[0]) {
+                // intro
+                setHideDivs([false, true, true]);
+            } else if (window.scrollY < thresholds[1]) {
+                // about me
+                setHideDivs([true, false, true]);
+            } else if (window.scrollY < thresholds[2]) {
+                // my projects
+                setHideDivs([true, true, false]);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
         <div className="page">
             <Header/>
-            <div className="homepage-wrapper">
-                <div className="homepage-titlediv">
-                    <div className="homepage-title">
-                        XINI
+            <div className="homepage-container">
+                <div className="homepage-blackbkg"></div>
+                <div className={`homepage-background ${fadeDivs[2] ? 'homepage-fade-bkg visible' : 'homepage-fade-bkg'}`}></div>
+                <div className="homepage-intro-container">
+                    <div className="homepage-titlediv">
+                        <div className={`homepage-title ${fadeDivs[0] ? (hideDivs[0] ? 'homepage-fade' : 'homepage-fade visible') : 'homepage-fade'}`}>
+                            XINI
+                        </div>
+                        <div className={`homepage-subtitle ${fadeDivs[1] ? (hideDivs[0] ? 'homepage-fade' : 'homepage-fade visible') : 'homepage-fade'}`}>
+                            <hr/>
+                            Designer • Developer • Creator
+                            <hr/>
+                        </div>
                     </div>
-                    <div className="homepage-subtitle">
-                        <hr className="homepage-titleline"/>
-                        Designer • Developer • Creator
+                </div>
+                <div className={`panel-title ${hideDivs[1] ? '' : 'visible'}`}>
+                    ABOUT ME
+                </div>
+                <div className={`panel-container ${hideDivs[1] ? '' : 'visible'}`}>
+                    <div className="about-img">
+                        <img src={Xini_Icon_White} alt="About Img"/>
                     </div>
+                    <div className="about-intro">
+                        Welcome to xini.dev! This is Anthony :&#41;
+                    </div>
+                    <div className="about-description">
+                        I am a software developer, and a computer scientist graduate from the University of Warwick.
+                        I have a deep passion for coding, especially for game development.
+                    </div>
+                    <div className="about-remarks">
+                        Join me and step towards the future with creativity and innovation.
+                    </div>
+                </div>
+                <div className={`panel-title ${hideDivs[2] ? '' : 'visible'}`}>
+                    MY PROJECTS
+                </div>
+                <div className={`panel-container ${hideDivs[2] ? '' : 'visible'}`}>
                 </div>
             </div>
         </div>
